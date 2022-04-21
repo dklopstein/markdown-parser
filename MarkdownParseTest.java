50c99548a6d879a857e4ed0fecd2b79338b3cc18
dklopstein <dklopstein@ucsd.edu>
2022-04-21 11:04:49 -0700

    added JUnit tests for other .md files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,13 +12,52 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
 
-    @Test
-    public void testGetLinks() throws IOException {
-        Path fileName = Path.of("test-file.md");
+    public ArrayList<String> links(String file) throws IOException {
+        Path fileName = Path.of(file);
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
+        return links;
+    }
+
+    @Test
+    public void getLinksTestFile() throws IOException {
         List<String> expected = List.of("https://something.com", "some-thing.html");
 
-        assertEquals(expected, links);
+        assertEquals(expected, links("test-file.md"));
+    }
+
+    @Test
+    public void getLinksOnlyBrackets() throws IOException {
+        List<String> expected = List.of();
+
+        assertEquals(expected, links("only-brackets.md"));
+    }
+
+    @Test
+    public void getLinksOnlyParen() throws IOException {
+        List<String> expected = List.of();
+
+        assertEquals(expected, links("only-paren.md"));
+    }
+
+    @Test
+    public void getLinksFarSyntax() throws IOException {
+        List<String> expected = List.of();
+
+        assertEquals(expected, links("far-syntax.md"));
+    }
+
+    @Test
+    public void getLinksImageTest() throws IOException {
+        List<String> expected = List.of();
+
+        assertEquals(expected, links("image-test.md"));
+    }
+    
+    @Test
+    public void getLinksBreakingTest() throws IOException {
+        List<String> expected = List.of();
+
+        assertEquals(expected, links("breaking-test.md"));
     }
 }
