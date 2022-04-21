ebba02eaf2d3373871fc75016e97bfeabe4c788d
dklopstein <dklopstein@ucsd.edu>
2022-04-21 11:45:51 -0700

    remove repeated lines and fixed a small +1 error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -23,16 +23,13 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             // fixed image being read as a link
-            if (markdown.indexOf("!", currentIndex) == openBracket - 1) {
-                currentIndex = closeParen + 1;
-                continue;
-            }
             // fixed distance between [] and ()
-            if (closeBracket + 1 != openParen) {
+            if (markdown.indexOf("!", currentIndex) == openBracket - 1 ||
+                closeBracket + 1 != openParen) {
                 currentIndex = closeParen;
                 continue;
             }
-
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1; 
             
